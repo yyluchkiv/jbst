d8d4cd82590976a92e03816555dbe424785f7730
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-12-27 11:02:41 +0000

    —

--- jbst-foundation/src/main/java/jbst/foundation/feigns/openai/OpenaiClient.java
+++ jbst-foundation/src/main/java/jbst/foundation/feigns/openai/OpenaiClient.java
@@ -1,14 +1,15 @@
 package jbst.foundation.feigns.openai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
-import jbst.foundation.feigns.openai.domain.requests.OpenaiCompletionsRequest;
-import jbst.foundation.feigns.openai.domain.responses.OpenaiCompletionsResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 @Slf4j
@@ -29,6 +30,56 @@ public class OpenaiClient {
                 OpenaiCompletionsRequest request
         );
     }
+
+    // Classes: Requests
+    public record OpenaiCompletionsRequest(
+            String model,
+            String prompt,
+            @JsonProperty("max_tokens") int maxTokens,
+            double temperature
+    ) {
+
+        @SuppressWarnings("unused")
+        public static OpenaiCompletionsRequest davinci003(String prompt) {
+            return new OpenaiCompletionsRequest(
+                    "text-davinci-003",
+                    prompt,
+                    4000,
+                    1.0d
+            );
+        }
+    }
+
+    // Classes: Responses
+    public record OpenaiCompletionsResponse(
+            String id,
+            String object,
+            long created,
+            String model,
+            List<OpenaiCompletionsChoiceResponse> choices,
+            OpenaiCompletionsUsageResponse usage
+    ) {
+    }
+
+    public record OpenaiCompletionsChoiceResponse(
+            String text,
+            int index,
+            String logprobs,
+            @JsonProperty("finish_reason")
+            String finishReason
+    ) {
+    }
+
+    public record OpenaiCompletionsUsageResponse(
+            @JsonProperty("prompt_tokens")
+            int promptTokens,
+            @JsonProperty("completion_tokens")
+            int completionTokens,
+            @JsonProperty("total_tokens")
+            int totalTokens
+    ) {
+    }
+
 
     // Definitions
     private final OpenaiDefinition definition;
