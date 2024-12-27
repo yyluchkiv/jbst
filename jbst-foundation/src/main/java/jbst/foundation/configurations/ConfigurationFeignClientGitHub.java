7d50024d94ae05fffa11cd139c7f0e2e5ac08e51
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-12-27 10:11:30 +0000

    simplify utils: github

--- jbst-foundation/src/main/java/jbst/foundation/configurations/ConfigurationFeignClientGitHub.java
+++ jbst-foundation/src/main/java/jbst/foundation/configurations/ConfigurationFeignClientGitHub.java
@@ -5,7 +5,6 @@ import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import jbst.foundation.feigns.github.GithubClient;
-import jbst.foundation.feigns.github.GithubDefinition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,16 +15,13 @@ import org.springframework.context.annotation.Configuration;
 public class ConfigurationFeignClientGitHub {
 
     @Bean
-    GithubDefinition githubDefinition() {
-        return Feign.builder()
-                .client(new OkHttpClient())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(GithubDefinition.class, "https://api.github.com");
-    }
-
-    @Bean
     GithubClient githubClient() {
-        return new GithubClient(this.githubDefinition());
+        return new GithubClient(
+                Feign.builder()
+                        .client(new OkHttpClient())
+                        .encoder(new JacksonEncoder())
+                        .decoder(new JacksonDecoder())
+                        .target(GithubClient.GithubDefinition.class, "https://api.github.com")
+        );
     }
 }
--- jbst-foundation/src/main/java/jbst/foundation/feigns/github/domain/requests/GithubRepoContentsRequest.java
+++
@@ -1,9 +0,0 @@
-package jbst.foundation.feigns.github.domain.requests;
-
-public record GithubRepoContentsRequest(
-        String token,
-        String owner,
-        String repo,
-        String content
-) {
-}
