24364d4800618e73bb40bae2550f5be73f9c7a53
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-12-27 10:09:20 +0000

    simplify utils: openai

--- jbst-foundation/src/main/java/jbst/foundation/configurations/ConfigurationFeignClientOpenai.java
+++ jbst-foundation/src/main/java/jbst/foundation/configurations/ConfigurationFeignClientOpenai.java
@@ -5,7 +5,6 @@ import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import jbst.foundation.feigns.openai.OpenaiClient;
-import jbst.foundation.feigns.openai.OpenaiDefinition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,16 +15,13 @@ import org.springframework.context.annotation.Configuration;
 public class ConfigurationFeignClientOpenai {
 
     @Bean
-    OpenaiDefinition openaiDefinition() {
-        return Feign.builder()
-                .client(new OkHttpClient())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(OpenaiDefinition.class, "https://api.openai.com");
-    }
-
-    @Bean
     OpenaiClient openaiClient() {
-        return new OpenaiClient(this.openaiDefinition());
+        return new OpenaiClient(
+                Feign.builder()
+                        .client(new OkHttpClient())
+                        .encoder(new JacksonEncoder())
+                        .decoder(new JacksonDecoder())
+                        .target(OpenaiClient.OpenaiDefinition.class, "https://api.openai.com")
+        );
     }
 }
--- jbst-foundation/src/main/java/jbst/foundation/feigns/openai/OpenaiClient.java
+++ jbst-foundation/src/main/java/jbst/foundation/feigns/openai/OpenaiClient.java
@@ -1,16 +1,34 @@
 package jbst.foundation.feigns.openai;
 
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
 import jbst.foundation.feigns.openai.domain.requests.OpenaiCompletionsRequest;
 import jbst.foundation.feigns.openai.domain.responses.OpenaiCompletionsResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.MediaType;
 
 @SuppressWarnings("unused")
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class OpenaiClient {
+
+    // Classes: Definitions
+    public interface OpenaiDefinition {
+        @RequestLine("POST /v1/completions")
+        @Headers(
+                {
+                        "Authorization: Bearer {token}",
+                        "Content-Type: " + MediaType.APPLICATION_JSON_VALUE
+                }
+        )
+        OpenaiCompletionsResponse completions(
+                @Param("token") String token,
+                OpenaiCompletionsRequest request
+        );
+    }
 
     // Definitions
     private final OpenaiDefinition definition;
