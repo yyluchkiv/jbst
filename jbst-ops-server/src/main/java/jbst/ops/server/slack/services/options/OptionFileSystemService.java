64b5332ffae925e6da559c51b99b02634673f2c1
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-01-11 20:12:08 +0000

    ops-server #58

--- jbst-ops-server/src/main/java/jbst/ops/server/slack/services/options/OptionFileSystemService.java
+++ jbst-ops-server/src/main/java/jbst/ops/server/slack/services/options/OptionFileSystemService.java
@@ -5,7 +5,6 @@ import jbst.ops.server.domain.slack.messages.SlackMessageType;
 import jbst.ops.server.domain.slack.requests.SlackRequestContext;
 
 public interface OptionFileSystemService {
-    void sendFsTables(SlackRequestContext slackRequestContext);
     void sendFsStatusOnSshRequiredAnyProblemsOnFsMetadata(SlackRequestContext slackRequestContext);
     void sendFsStatusOrFailures(SlackRequestContext slackRequestContext, Servers servers, SlackMessageType slackMessageType);
     void sendFsFailures(SlackRequestContext slackRequestContext, Servers servers, SlackMessageType slackMessageType);
--- jbst-ops-server/src/main/java/jbst/ops/server/slack/services/options/impl/OptionFileSystemServiceImpl.java
+++ jbst-ops-server/src/main/java/jbst/ops/server/slack/services/options/impl/OptionFileSystemServiceImpl.java
@@ -1,7 +1,5 @@
 package jbst.ops.server.slack.services.options.impl;
 
-import jbst.foundation.domain.collections.Partitions;
-import jbst.ops.server.domain.servers.FileSystemMetadataRow;
 import jbst.ops.server.domain.servers.Servers;
 import jbst.ops.server.domain.slack.messages.SlackMessageFileSystemTable;
 import jbst.ops.server.domain.slack.messages.SlackMessageType;
@@ -16,13 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static jbst.ops.server.constants.OpsConstants.Services.FILE_SYSTEM_SERVICE;
-import static jbst.ops.server.domain.servers.FileSystemMetadataRow.PERCENTAGE_REVERSED;
-import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Slf4j
 @Component
@@ -33,64 +27,6 @@ public class OptionFileSystemServiceImpl implements OptionFileSystemService {
     private final MonitoringService monitoringService;
     // Messaging
     private final SlackMessagingService slackMessagingService;
-
-    @Override
-    public void sendFsTables(SlackRequestContext slackRequestContext) {
-        var servers = this.monitoringService.getServersSshRequired();
-
-        List<String> warningTables = new ArrayList<>();
-        List<FileSystemMetadataRow> successesRows = new ArrayList<>();
-
-        servers.getValues().forEach(server -> {
-            if (server.fileSystemMetadataProblems()) {
-                warningTables.add(new SlackMessageFileSystemTable(server).getValue());
-            } else if (server.fileSystemMetadata().isAnyRows()) {
-                successesRows.addAll(server.fileSystemMetadata().rows());
-            }
-        });
-
-        if (!isEmpty(successesRows)) {
-//            this.slackMessagingService.sendAsync(
-//                    channelSlackMessage(
-//                            slackRequestContext,
-//                            Operation.FS_TABLES.getMessage()
-//                    )
-//            );
-            successesRows.sort(PERCENTAGE_REVERSED);
-            // WARNING: 25 is practical number is this case as max slack rows to wrap a message
-            var partitionsSuccesses = Partitions.ofSize(successesRows, 25);
-            partitionsSuccesses.forEach(chuckedMappedRows -> {
-                var table = new SlackMessageFileSystemTable(chuckedMappedRows).getValue();
-//                this.slackMessagingService.sendAsync(
-//                        channelSlackMessage(
-//                                slackRequestContext,
-//                                table
-//                        )
-//                );
-            });
-        }
-
-        if (!isEmpty(warningTables)) {
-            warningTables.add(0, MessagesUtility.getResponseWarnings());
-//            this.slackMessagingService.sendAsync(
-//                    channelSlackMessages(
-//                            slackRequestContext,
-//                            warningTables
-//                    )
-//            );
-        }
-
-        if (isEmpty(successesRows) && isEmpty(warningTables)) {
-            List<String> messages = new ArrayList<>();
-            messages.add(SlackMessageFileSystemTable.getNoFsTable());
-//            this.slackMessagingService.sendAsync(
-//                    channelSlackMessages(
-//                            slackRequestContext,
-//                            messages
-//                    )
-//            );
-        }
-    }
 
     @Override
     public void sendFsStatusOnSshRequiredAnyProblemsOnFsMetadata(SlackRequestContext slackRequestContext) {
