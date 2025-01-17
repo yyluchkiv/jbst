18824c620773c024c730f990e118f3556ff898e6
Volodymyr Bolshakov <volodbol@gmail.com>
2025-01-17 17:04:34 +0200

    fix logs formatting

--- jbst-ops-server/src/main/java/jbst/ops/server/domain/computed/ServerInfinityTimerTask.java
+++ jbst-ops-server/src/main/java/jbst/ops/server/domain/computed/ServerInfinityTimerTask.java
@@ -363,7 +363,7 @@ public class ServerInfinityTimerTask {
     }
 
     private void ssh() throws SshSessionException {
-        LOGGER.info(PREFIX + "SSH into server {}. Status: {}", this.getName(), STARTED.formatAnsi());
+        LOGGER.info(PREFIX + " SSH into server {}. Status: {}", this.getName(), STARTED.formatAnsi());
         var sshSession = SshUtility.getSession(this.serverSshConfigs.getConnectionConfigs());
         if (sshSession.getSession().present()) {
             this.sshLastUpdatedAt = getCurrentTimestamp();
@@ -386,7 +386,7 @@ public class ServerInfinityTimerTask {
         } else {
             this.fileSystemMetadata = ServerFileSystemMetadata.failure(sshSession.getThrowable().value());
         }
-        LOGGER.info(PREFIX + "SSH into server {}. Status: {}", this.getName(), COMPLETED.formatAnsi());
+        LOGGER.info(PREFIX + " SSH into server {}. Status: {}", this.getName(), COMPLETED.formatAnsi());
     }
 
     // ================================================================================================================
