9af8a5d2866dc9d2ce5e75078708343829a7631a
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-07-29 08:48:49 +0100

    — Modification: PropertiesPrinter codebase

--- jbst-foundation/src/main/java/jbst/foundation/domain/properties/base/AbstractPropertyConfigs.java
+++ jbst-foundation/src/main/java/jbst/foundation/domain/properties/base/AbstractPropertyConfigs.java
@@ -2,8 +2,10 @@ package jbst.foundation.domain.properties.base;
 
 import jbst.foundation.domain.base.PropertyId;
 import jbst.foundation.domain.properties.utilities.PropertiesAsserter;
-import jbst.foundation.domain.properties.utilities.PropertiesPrinter;
+import jbst.foundation.domain.reflections.ReflectionProperty;
 import lombok.extern.slf4j.Slf4j;
+
+import static jbst.foundation.utilities.reflections.ReflectionUtility.getProperties;
 
 @Slf4j
 public abstract class AbstractPropertyConfigs {
@@ -13,6 +15,16 @@ public abstract class AbstractPropertyConfigs {
     }
 
     public void printProperties(PropertyId propertyId) {
-        PropertiesPrinter.printMandatoryBasedConfigs(this, propertyId);
+        this.printMandatoryBasedConfigs(propertyId);
+    }
+
+    // =================================================================================================================
+    // PRIVATE METHODS
+    // =================================================================================================================
+    private void printMandatoryBasedConfigs(PropertyId propertyId) {
+        var fields = PropertiesAsserter.getMandatoryBasedFields(this, propertyId);
+        var rfs = getProperties(this, propertyId, fields);
+        rfs.sort(ReflectionProperty.PRINTER_COMPARATOR);
+        rfs.forEach(ReflectionProperty::print);
     }
 }
