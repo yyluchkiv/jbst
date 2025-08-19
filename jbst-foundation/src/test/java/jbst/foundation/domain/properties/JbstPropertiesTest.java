75dd712a1cfee1c33126a54d834576346e0e37b9
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-08-19 19:10:56 +0100

    — Addition: MagicLink authentication

--- jbst-foundation/src/test/java/jbst/foundation/domain/properties/JbstPropertiesTest.java
+++ jbst-foundation/src/test/java/jbst/foundation/domain/properties/JbstPropertiesTest.java
@@ -2,11 +2,14 @@ package jbst.foundation.domain.properties;
 
 import jbst.foundation.configurations.TestJbstConfigurationPropertiesHardcoded;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 import java.lang.reflect.InvocationTargetException;
 
 import static jbst.foundation.utilities.reflections.ReflectionUtility.getGetters;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class JbstPropertiesTest {
 
@@ -39,5 +42,23 @@ class JbstPropertiesTest {
 
         // Assert
         assertThat(link).isEqualTo("http://127.0.0.1:3000/email-confirmation");
+    }
+
+    @Test
+    void getEmailConfirmationLink() {
+        // Act
+        var contextPath = "/tests-context-path";
+        var servlet = mock(ServerProperties.Servlet.class);
+        var serverProperties = mock(ServerProperties.class);
+        when(servlet.getContextPath()).thenReturn(contextPath);
+        when(serverProperties.getServlet()).thenReturn(servlet);
+        var token = "9F2FCF5EFC2A026B319D5D267C06D8D06B0C18C1";
+        var link = this.jbstProperties.getEmailConfirmationLink(
+                serverProperties,
+                token
+        );
+
+        // Assert
+        assertThat(link).isEqualTo("http://127.0.0.1:3002/tests-context-path/jbst/security/tokens/email/confirm?token=" + token);
     }
 }
