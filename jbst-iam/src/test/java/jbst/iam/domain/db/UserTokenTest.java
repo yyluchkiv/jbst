4ee0c02ce5bdd532c4805493e4811c8c0679842d
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-08-20 13:13:26 +0100

    — Addition: MagicLink authentication

--- jbst-iam/src/test/java/jbst/iam/domain/db/UserTokenTest.java
+++ jbst-iam/src/test/java/jbst/iam/domain/db/UserTokenTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static jbst.foundation.utilities.time.TimestampUtility.getCurrentTimestamp;
+import static jbst.iam.domain.enums.UserTokenType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTokenTest {
@@ -27,6 +28,26 @@ class UserTokenTest {
         );
     }
 
+    private static Stream<Arguments> isInvalidCases() {
+        var currentTimestamp = getCurrentTimestamp();
+        var pastTimestamp = currentTimestamp - 1000; // 1 second in the past
+        var futureTimestamp = currentTimestamp + 1000; // 1 second in the future
+
+        return Stream.of(
+                Arguments.of(MAGIC_LINK, MAGIC_LINK, false, futureTimestamp, false),
+                Arguments.of(EMAIL_CONFIRMATION, MAGIC_LINK, false, futureTimestamp, true),
+                Arguments.of(MAGIC_LINK, MAGIC_LINK, true, futureTimestamp, true),
+                Arguments.of(MAGIC_LINK, MAGIC_LINK, false, pastTimestamp, true),
+                Arguments.of(PASSWORD_RESET, MAGIC_LINK, true, futureTimestamp, true),
+                Arguments.of(EMAIL_CONFIRMATION, MAGIC_LINK, false, pastTimestamp, true),
+                Arguments.of(MAGIC_LINK, MAGIC_LINK, true, pastTimestamp, true),
+                Arguments.of(PASSWORD_RESET, MAGIC_LINK, true, pastTimestamp, true),
+                Arguments.of(PASSWORD_RESET, EMAIL_CONFIRMATION, false, futureTimestamp, true),
+                Arguments.of(EMAIL_CONFIRMATION, EMAIL_CONFIRMATION, false, futureTimestamp, false),
+                Arguments.of(PASSWORD_RESET, PASSWORD_RESET, false, futureTimestamp, false)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("isExpiredCases")
     void isExpiredTest(long expiryTimestamp, boolean expected) {
@@ -35,12 +56,29 @@ class UserTokenTest {
                 TokenId.hardcoded(),
                 Email.hardcoded(),
                 "test-token-value",
-                UserTokenType.MAGIC_LINK,
+                MAGIC_LINK,
                 expiryTimestamp,
                 false
         );
 
         // Act + Assert
         assertThat(userToken.isExpired()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("isInvalidCases")
+    void isInvalidTest(UserTokenType tokenType, UserTokenType expectedType, boolean used, long expiryTimestamp, boolean expected) {
+        // Arrange
+        var userToken = new UserToken(
+                TokenId.hardcoded(),
+                Email.hardcoded(),
+                "test-token-value",
+                tokenType,
+                expiryTimestamp,
+                used
+        );
+
+        // Act + Assert
+        assertThat(userToken.isInvalid(expectedType)).isEqualTo(expected);
     }
 }
