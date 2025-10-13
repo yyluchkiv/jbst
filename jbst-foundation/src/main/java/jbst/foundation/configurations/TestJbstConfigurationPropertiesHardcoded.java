4706c01b1a485be42cf7d8c90128304cd038ed47
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-10-13 20:11:39 +0100

    —

--- jbst-foundation/src/main/java/jbst/foundation/configurations/TestJbstConfigurationPropertiesHardcoded.java
+++ jbst-foundation/src/main/java/jbst/foundation/configurations/TestJbstConfigurationPropertiesHardcoded.java
@@ -1,11 +1,9 @@
 package jbst.foundation.configurations;
 
-import jbst.foundation.domain.annotations.JbstDeletionScheduled;
 import jbst.foundation.domain.properties.JbstProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@JbstDeletionScheduled(version = "1.23")
 @Configuration
 public class TestJbstConfigurationPropertiesHardcoded {
 
