26c3560a5cb53df7d8f15371bf50b07250b73c2d
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-10-21 11:29:19 +0100

    — Addition: AbstractPropertiesConfigs.getPropertyName()

--- jbst-foundation/src/main/java/jbst/foundation/domain/properties/utilities/PropertiesAsserter.java
+++ jbst-foundation/src/main/java/jbst/foundation/domain/properties/utilities/PropertiesAsserter.java
@@ -1,11 +1,8 @@
 package jbst.foundation.domain.properties.utilities;
 
-import jbst.foundation.domain.asserts.ConsoleAsserts;
 import jbst.foundation.domain.properties.annotations.MandatoryProperty;
 import jbst.foundation.domain.properties.annotations.MandatoryToggleProperty;
 import jbst.foundation.domain.properties.annotations.NonMandatoryProperty;
-import jbst.foundation.domain.properties.base.AbstractPropertyConfigs;
-import jbst.foundation.domain.reflections.JbstProperty;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,22 +33,5 @@ public class PropertiesAsserter {
     public static List<Field> getMandatoryBasedFields(Object property, String propertyName) {
         assertNonNullOrThrow(property, propertyName);
         return getFields(property, Set.of(MandatoryProperty.class, NonMandatoryProperty.class, MandatoryToggleProperty.class));
-    }
-
-    // =================================================================================================================
-    // PRIVATE METHODS
-    // =================================================================================================================
-
-    private static void assertPropertyConfigs(AbstractPropertyConfigs propertyConfigs, String propertyName, List<Field> fields) {
-        assertNonNullOrThrow(propertyConfigs, propertyName);
-        fields.forEach(field -> {
-            try {
-                var jbstProperty = new JbstProperty(propertyName, field, field.get(propertyConfigs));
-                ConsoleAsserts.assertNonNullOrThrow(jbstProperty);
-                jbstProperty.verify();
-            } catch (IllegalAccessException ex) {
-                throw new IllegalArgumentException(ex);
-            }
-        });
     }
 }
