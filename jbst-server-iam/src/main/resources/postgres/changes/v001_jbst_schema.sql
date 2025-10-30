3ceb5fb14264feb1940cfd555d5a58ec41572ea4
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2025-10-30 18:16:06 +0000

    —

--- jbst-server-iam/src/main/resources/postgres/changes/v001_jbst_schema.sql
+++ jbst-server-iam/src/main/resources/postgres/changes/v001_jbst_schema.sql
@@ -28,56 +28,56 @@ VALUES (
 );
 
 CREATE TABLE "jbst_users" (
-    "id" varchar(36) PRIMARY KEY,
-    "creation_option" varchar(255) NOT NULL,
-    "username" varchar(255) NOT NULL,
-    "password" varchar(255) NOT NULL,
-    "enabled" bool NOT NULL,
-    "zone_id" varchar(255) NOT NULL,
-    "authorities" varchar(1024) NOT NULL,
-    "email" varchar(255),
-    "name" varchar(255),
-    "password_change_required" bool NOT NULL,
-    "email_details" jsonb NOT NULL,
-    "attributes" varchar(65535)
+    "id"                                varchar(36) PRIMARY KEY,
+    "creation_option"                   varchar(255) NOT NULL,
+    "username"                          varchar(255) NOT NULL,
+    "password"                          varchar(255) NOT NULL,
+    "enabled"                           bool NOT NULL,
+    "zone_id"                           varchar(255) NOT NULL,
+    "authorities"                       varchar(1024) NOT NULL,
+    "email"                             varchar(255),
+    "name"                              varchar(255),
+    "password_change_required"          bool NOT NULL,
+    "email_details"                     jsonb NOT NULL,
+    "attributes"                        varchar(65535)
 );
 
 CREATE TABLE "jbst_users_sessions" (
-    "id" varchar(36) PRIMARY KEY,
-    "created_at" int8 NOT NULL,
-    "updated_at" int8 NOT NULL,
-    "username" varchar(255) NOT NULL,
-    "access_token" varchar(4096) NOT NULL,
-    "refresh_token" varchar(4096) NOT NULL,
-    "metadata" varchar(65535) NOT NULL,
-    "metadata_renew_cron" bool NOT NULL,
-    "metadata_renew_manually" bool NOT NULL
+    "id"                                varchar(36) PRIMARY KEY,
+    "created_at"                        int8 NOT NULL,
+    "updated_at"                        int8 NOT NULL,
+    "username"                          varchar(255) NOT NULL,
+    "access_token"                      varchar(4096) NOT NULL,
+    "refresh_token"                     varchar(4096) NOT NULL,
+    "metadata"                          varchar(65535) NOT NULL,
+    "metadata_renew_cron"               bool NOT NULL,
+    "metadata_renew_manually"           bool NOT NULL
 );
 
 CREATE TABLE "jbst_users_tokens" (
-    "id" varchar(36) PRIMARY KEY,
-    "email" varchar(255) NOT NULL,
-    "username" varchar(255) NOT NULL,
-    "value" varchar(255) NOT NULL,
-    "type" varchar(255) NOT NULL,
-    "expiry_timestamp" int8 NOT NULL,
-    "used" bool NOT NULL
+    "id"                                varchar(36) PRIMARY KEY,
+    "email"                             varchar(255) NOT NULL,
+    "username"                          varchar(255) NOT NULL,
+    "value"                             varchar(255) NOT NULL,
+    "type"                              varchar(255) NOT NULL,
+    "expiry_timestamp"                  int8 NOT NULL,
+    "used"                              bool NOT NULL
 );
 
 CREATE TABLE "jbst_invitations" (
-    "id" varchar(36),
-    "owner" varchar(255) NOT NULL,
-    "authorities" varchar(1024) NOT NULL,
-    "code" varchar(40) NOT NULL,
-    "invited" varchar(255)
+    "id"                                varchar(36) PRIMARY KEY,
+    "owner"                             varchar(255) NOT NULL,
+    "authorities"                       varchar(1024) NOT NULL,
+    "code"                              varchar(40) NOT NULL,
+    "invited"                           varchar(255)
 );
 
 -- =================================================================================================================
 -- SERVER
 -- =================================================================================================================
 CREATE TABLE "anything" (
-    "id" varchar(36) PRIMARY KEY,
-    "username" varchar(255) NOT NULL,
-    "value" varchar
+    "id"                                varchar(36) PRIMARY KEY,
+    "username"                          varchar(255) NOT NULL,
+    "value"                             varchar
 );
 
