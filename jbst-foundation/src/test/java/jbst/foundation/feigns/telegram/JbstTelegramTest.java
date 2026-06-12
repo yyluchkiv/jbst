081dfe3657f8c3eb3951baab92010a8bdfb9d275
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2026-06-12 12:44:03 +0100

    test: add trace/backpressure to jbst-telegram

--- jbst-foundation/src/test/java/jbst/foundation/feigns/telegram/JbstTelegramTest.java
+++ jbst-foundation/src/test/java/jbst/foundation/feigns/telegram/JbstTelegramTest.java
@@ -2,6 +2,7 @@ package jbst.foundation.feigns.telegram;
 
 import jbst.foundation.configurations.JbstConfigurationFeignClientTelegram;
 import jbst.foundation.domain.concurrent.JbstSleep;
+import jbst.foundation.domain.strings.JbstTraces;
 import jbst.foundation.feigns.telegram.JbstTelegram.ClientException;
 import jbst.foundation.feigns.telegram.JbstTelegram.ConfigurationException;
 import jbst.foundation.feigns.telegram.JbstTelegram.RateLimitsException;
@@ -19,6 +20,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+
 @Slf4j
 @ExtendWith({ SpringExtension.class })
 @ContextConfiguration(loader= AnnotationConfigContextLoader.class)
@@ -33,6 +35,8 @@ class JbstTelegramTest {
     }
 
     private static final String TELEGRAM_TOKEN = "-";
+    // How to obtain chatId
+    // Open: https://api.telegram.org/bot$token/getUpdates
     private static final String TELEGRAM_CHAT_ID = "-";
 
     private final JbstTelegram telegram;
@@ -48,9 +52,11 @@ class JbstTelegramTest {
     @Test
     void sendMessage() throws ConfigurationException, RateLimitsException, ClientException {
         // Arrange
+        var npe = new NullPointerException("jbst-telegram");
+        var trace = JbstTraces.getTrace(npe).value().substring(0, 4000);
         var message = JbstTelegram.TelegramMessageRequest.of(
                 TELEGRAM_CHAT_ID,
-                "<@username> <b>V1</b>"
+                trace
         );
 
         // Act
